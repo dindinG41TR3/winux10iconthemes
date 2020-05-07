a443baaa85a53f35bd923df442a285d2a5abc047
Dindin Hernawan <dindin_hernawan@yahoo.com>
2020-05-08 00:25:57 +0800

    Upload Winux10 1.0

---
+++ INSTALL.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+cd icons
+sudo cp Winux10 /usr/share/icons/
+cd ..
+cd themes
+sudo cp Winux10 /usr/share/themes/
+echo '===================================='
+echo ' OK DONE INSTALL SUKSES '
+echo '===================================='
